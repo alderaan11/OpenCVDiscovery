2a07db578a0c422bd44d93e6886a3ffaaca90ac8
Alderaan11 <anais.mackow@etu.umontpellier.fr>
2024-10-17 12:38:11 +0200

    Grey pic

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ import numpy as np
 import cv2
 
 
+img = cv2.imread("salomon-shoes.jpg",0)
+img2 = cv2.imread("salomon-shoes.jpg")
+
+#cv2.imshow('image', img)
+#cv2.imshow('image2', img2)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+cv2.imwrite("salomon-shoesG.jpg", img)
